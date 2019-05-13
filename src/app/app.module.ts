bcd402f214e56f5e55d8b6fe51bcb077629c0768
RADWIN_DOMAIN\yinon_b <yinon_b@radwin.com>
2019-05-13 15:38:14 +0300

    initial site

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CellComponent } from './components/cell/cell.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CellComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
